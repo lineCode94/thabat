04501a1a83994d55649b0aadb047edb6d83d5162
lineCode94 <lineCode94@gmail.com>
2026-07-24 01:38:19 +0300

    ngrok avalible

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig({
   },
   server: {
     port: 5173,
+    allowedHosts: ['grimace-drove-exact.ngrok-free.dev'],
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
